c19d0bfa4344737e55c70d08cc61e8519c3c8236
todikun <todirahmat123@gmail.com>
2022-06-09 16:27:04 +0700

    update launcher

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ Aplikasi deteksi gempa terkini
 from gempaterkini import GempaTerkini
 
 if __name__ == '__main__':
+    print('Dashboard monitoring')
     gempa = GempaTerkini('https://www.bmkg.go.id/')
     gempa.show_description()
     gempa.run()
